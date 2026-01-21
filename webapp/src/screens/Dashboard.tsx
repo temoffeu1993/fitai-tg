82eea06efdcb4310020aee1e5104aa5870ce6fd1
temoffeu19 <kindersalon76@gmail.com>
2026-01-21 22:50:15 +0300

    Update welcome title text

--- webapp/src/screens/Dashboard.tsx
+++ webapp/src/screens/Dashboard.tsx
@@ -341,8 +341,7 @@ export default function Dashboard() {
         <section style={s.introFooter} className="intro-fade intro-fade-delay-2">
           <div style={s.introTextBlock}>
             <h1 style={s.introTitle}>
-              <span style={s.introTitleLine}>Добро пожаловать</span>
-              <span style={s.introTitleLine}>в Моро</span>
+              <span style={s.introTitleLine}>Тренеруйся с Моро</span>
             </h1>
             <p style={s.introSubtitle}>
               Умный фитнес в одном месте: тренировки, прогрессия, питание и рекомендации
